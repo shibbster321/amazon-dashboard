e07ed525b9c713b43e69344f7edef314f071294d
cameron brock <cameronb@usc.edu>
2021-01-08 14:37:47 -0600

    comment out keys

--- config/initializers/peddler.rb
+++ config/initializers/peddler.rb
@@ -1,3 +1,3 @@
-AWS_ACCESS_KEY_ID =  Rails.application.credentials.aws[:access_key_id].freeze
-AWS_SECRET_ACCESS_KEY = Rails.application.credentials.aws[:secret_access_key].freeze
-MERCHANT_TOKEN = Rails.application.credentials.aws[:merchant_key].freeze
+# AWS_ACCESS_KEY_ID =  Rails.application.credentials.aws[:access_key_id].freeze
+# AWS_SECRET_ACCESS_KEY = Rails.application.credentials.aws[:secret_access_key].freeze
+# MERCHANT_TOKEN = Rails.application.credentials.aws[:merchant_key].freeze
